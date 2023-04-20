56f003e8b8e2f851989e9bcbd6066ef57a47ea2c
John Doe <johndoe@example.com>
2023-04-20 16:32:44 -0400

    change to old histogram code 2 (histogram no pdf) (was correct)

--- test.py
+++ test.py
@@ -9,43 +9,17 @@ test_lognormal_path = "datasets/lognormal-190M.bin.data"
 lognormal_path = "/users/ipatel9/dataset/lognormal-190M.bin.data"
 longlat_path = "/users/ipatel9/dataset/longlat-200M.bin.data"
 
-file_name = test_lognormal_path
+file_name = lognormal_path
 print("opening file: " + file_name)
 
 # read binary
-f = open(file_name, "rb")
-
-# read text
-# f_text = open(file_name, "r")
-
-print("creating float array...")
-# arr = np.fromfile(f_text, dtype=np.int)
-arr = f.read().split()
-# my_arr = np.load(lognormal_path)
-# print(arr)
-
-# arr = np.fromfile(f_text, dtype=np.float)
-print("sorting array...")
-arr.sort()
-
-key_type = int
-
-rand_dataset = np.random.randn(10000)  # generate samples from normal distribution (discrete data)
-rand_dataset.sort()
-if file_name is lognormal_path or test_lognormal_path:
-    key_type = np.int_
-elif file_name is longlat_path:
-    key_type = np.float
-
-# randArr = np.random.randint(0, 10, 5)
-# randArr.sort()
 with open(file_name, 'rb') as file:
-    arr = np.fromfile(file, dtype=key_type)
+    arr = np.fromfile(file, dtype=np.int_)
+
 
 # for i in range(10):
 #     print(arr[i])
 
-cdf = scipy.stats.norm.cdf(arr)
 # arr = np.divide(arr, np.sum(arr))
 
 # plot the cdf
@@ -64,5 +38,5 @@ plt.show(block=True)
 plt.plot(bin_edges[1:], cdf)
 plt.xlabel('Key')
 plt.ylabel('CDF')
-# plt.savefig("CDFplot.png")
-plt.show()
+plt.savefig("testCDFplot.png")
+# plt.show()
