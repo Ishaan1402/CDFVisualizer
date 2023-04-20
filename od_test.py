7eb5d53d465644d0743f2406a95bb817865385ec
John Doe <johndoe@example.com>
2023-04-20 12:45:35 -0400

    created countFreq method

--- od_test.py
+++ od_test.py
@@ -8,7 +8,7 @@ test_longlat_path = "datasets/longlat-200M.bin.data"
 lognormal_path = "/users/ipatel9/dataset/lognormal-190M.bin.data"
 longlat_path = "/users/ipatel9/dataset/longlat-200M.bin.data"
 
-file_name = lognormal_path
+file_name = sequential_path
 key_type = int
 
 if file_name == lognormal_path or file_name == test_lognormal_path:
@@ -16,16 +16,36 @@ if file_name == lognormal_path or file_name == test_lognormal_path:
 elif file_name == longlat_path or file_name == test_longlat_path:
     key_type = np.float
 
-with open(file_name, 'rb') as file:
+with open(file_name, 'r') as file:
     arr = np.fromfile(file, dtype=key_type)
 
 
+def countFreq(array):
+    freq_dict = {}
+    for num in array:
+        if num in freq_dict:
+            freq_dict[num] += 1
+        else:
+            freq_dict[num] = 1
+
+    unique_nums = list(freq_dict.keys())
+    unique_nums.sort()
+
+    freq_arr = [0] * len(unique_nums)
+    for i, num in enumerate(unique_nums):
+        freq_arr[i] = freq_dict[num]
+
+    return freq_arr
+
+
 # Create array counting the occurrences of integers in arr and return their frequency at each index
+counts_arr = countFreq(arr)
+
+print(len(counts_arr))
 print(len(arr))
-counts_arr = np.bincount(arr)
 
 # Calculate the probability density function
-pdf = counts_arr / len(arr)
+pdf = np.divide(counts_arr, len(arr))
 
 # Calculate the cumulative distribution function
 cdf = np.cumsum(pdf)
