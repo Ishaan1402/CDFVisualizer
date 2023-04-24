8c4329b8617473e5f064a610e588b29a35c593c3
John Doe <johndoe@example.com>
2023-04-24 10:21:32 -0400

    editing font

--- od_test.py
+++ od_test.py
@@ -39,40 +39,7 @@ with open(file_name, 'rb') as file:
 # arr = np.loadtxt(file_name)
 
 
-# countFreq counts the frequencies of each unique key and inputs them into a new array where the
-# frequencies are listed for each ith number
-# def countFreq(array):
-#     freq_dict = {}
-#     for num in array:
-#         if num in freq_dict:
-#             freq_dict[num] += 1
-#         else:
-#             freq_dict[num] = 1
-#
-#     unique_nums = list(freq_dict.keys())
-#     unique_nums.sort()
-#
-#     freq_arr = [0] * len(unique_nums)
-#     for i, num in enumerate(unique_nums):
-#         freq_arr[i] = freq_dict[num]
-#
-#     return freq_arr
-
-
-# Create array counting the occurrences of integers in arr and return their frequency at each index
-# print("start countFreq")
-# counts_arr = countFreq(arr)
-
 print("length of arr: ", len(arr))
-# print(arr)
-# print("length of count_arrs: ", len(counts_arr))
-# print(counts_arr)
-
-# Calculate the probability density function
-# pdf = np.divide(counts_arr, len(arr))
-
-# Calculate the cumulative distribution function
-# cdf = np.cumsum(pdf)
 
 # Create a plot of the CDF
 ecdf = np.arange(1, len(arr)+1) / len(arr)
@@ -80,27 +47,22 @@ arr.sort()
 
 # Plot the eCDF
 plt.step(arr, ecdf)
-# plt.step(arr.sort(), cdf)
+
 title_font = {'family' : 'sans-serif',
-              'fontname': 'Avenir',
-        'fontweight' : 'bold',
-        'size'   : 25}
+        'fontweight' : 'extra bold',
+        'size'   : 23}
 
 axis_font = {'family' : 'sans-serif',
-             'fontname': 'Avenir',
-        'fontweight' : 'extra bold',
-        'size'   : 20}
+        'fontweight' : 'bold',
+        'size'   : 18}
 
-plt.title("Test Longlat Plot", **title_font)
+plt.title("longitudes", **title_font)
 plt.xlabel('Key', **axis_font)
 plt.ylabel('CDF', **axis_font)
 plt.subplots_adjust(bottom=0.15)
-locs, labels = plt.yticks()  # Get the current locations and labels.
+locs, labels = plt.yticks()
 plt.yticks(np.arange(0, 1.1, step=0.25))
 plt.grid(axis = 'y')
-plt.title('longitudes')
-plt.xlabel('Key')
-plt.ylabel('CDF')
 print("saving plot...")
 plt.savefig("longitudes_ecdf.png")
 # plt.show()
