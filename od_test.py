024abccb744e51d0ed12bf3a9ba29e15031109c2
John Doe <johndoe@example.com>
2023-04-24 11:45:39 -0400

    trying lognormal

--- od_test.py
+++ od_test.py
@@ -23,7 +23,7 @@ cit_path = "/users/ipatel9/dataset/cit-patents.out"
 fb_path = "/users/ipatel9/dataset/fb-wall.out"
 
 
-file_name = longlat_path
+file_name = lognormal_path
 key_type = np.int64
 
 if file_name == lognormal_path or file_name == test_lognormal_path or file_name == ycsb_path or file_name == test_ycsb_path:
@@ -65,5 +65,6 @@ locs, labels = plt.yticks()
 plt.yticks(np.arange(0, 1.1, step=0.25))
 plt.grid(axis = 'y')
 print("saving plot...")
-plt.savefig("longlat_ecdf.png")
+plt.savefig("lognormal_ecdf.png")
+print(file_name, " plot created")
 # plt.show()
