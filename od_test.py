46f29fa46aee0b63110f6e9601ae5138ca8b0603
John Doe <johndoe@example.com>
2023-04-24 01:29:25 -0400

    ready for longitudes

--- od_test.py
+++ od_test.py
@@ -1,18 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Test Paths
 sequential_path = "datasets/sequential-dataset.txt"
 test_lognormal_path = "datasets/lognormal-190M.bin.data"
 test_longlat_path = "datasets/longlat-200M.bin.data"
 test_longitudes_path = "datasets/longitudes-200M.bin.data"
 test_ycsb_path = "datasets/ycsb-200M.bin.data"
+test_amazon_path = "datasets/amazon.out.text"
+test_enron_path = "datasets/enron.out"
+test_cit_path = "datasets/cit-patents.out"
+test_fb_path = "datasets/fb-wall.out"
+
+# True Paths
 lognormal_path = "/users/ipatel9/dataset/lognormal-190M.bin.data"
 longlat_path = "/users/ipatel9/dataset/longlat-200M.bin.data"
 longitudes_path = "/users/ipatel9/dataset/longitudes-200M.bin.data"
 ycsb_path = "/users/ipatel9/dataset/ycsb-200M.bin.data"
-amazon_path = "datasets/amazon.out.text"
-test_enron_path = "datasets/enron.out"
 enron_path = "/users/ipatel9/dataset/enron.out"
+amazon_path = "/users/ipatel9/dataset/amazon.out.text"
+cit_path = "/users/ipatel9/dataset/cit-patents.out"
+fb_path = "/users/ipatel9/dataset/fb-wall.out"
+
 
 file_name = longitudes_path
 key_type = np.int64
@@ -72,15 +81,28 @@ arr.sort()
 # Plot the eCDF
 plt.step(arr, ecdf)
 # plt.step(arr.sort(), cdf)
+title_font = {'family' : 'sans-serif',
+              'fontname': 'Avenir',
+        'fontweight' : 'bold',
+        'size'   : 25}
+
+axis_font = {'family' : 'sans-serif',
+             'fontname': 'Avenir',
+        'fontweight' : 'extra bold',
+        'size'   : 20}
+
+plt.title("Test Longlat Plot", **title_font)
+plt.xlabel('Key', **axis_font)
+plt.ylabel('CDF', **axis_font)
+plt.subplots_adjust(bottom=0.15)
 locs, labels = plt.yticks()  # Get the current locations and labels.
 plt.yticks(np.arange(0, 1.1, step=0.25))
 plt.grid(axis = 'y')
-plt.title('Enron')
-plt.xscale("log")
+plt.title('longitudes')
 plt.xlabel('Key')
 plt.ylabel('CDF')
 print("saving plot...")
-plt.savefig("enron_ecdf.png")
+plt.savefig("longitudes_ecdf.png")
 # plt.show()
 
 
